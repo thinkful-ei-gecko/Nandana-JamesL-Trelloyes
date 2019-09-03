61141a1aed1d1f9fbea7a8d9cbf4f00c46ac4366
nrrao <nandanarao@yahoo.com>
2019-09-03 16:27:56 -0400

    created list

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -10,15 +10,46 @@ function App(props) {
   const cardList1 = props.store.lists[0].cardIds.map(id => {
      return props.store.allCards[id];
   });
+  const cardList2 = props.store.lists[1].cardIds.map(id => {
+    return props.store.allCards[id];
+ });
+ const cardList3 = props.store.lists[2].cardIds.map(id => {
+  return props.store.allCards[id];
+});
+const cardList4 = props.store.lists[3].cardIds.map(id => {
+  return props.store.allCards[id];
+});
 
   return (
-    <div>
+    <main class="App">
+    <header class="App-header">
+      <h1>Trelloyes!</h1>
+    </header>
+    <div class="App-list">
+      
       <List
+    
         header={props.store.lists[0].header} 
         cards={cardList1}
-        />
+      />
+      <List
+    
+    header={props.store.lists[1].header} 
+    cards={cardList2}
+  />
+     <List
+    
+    header={props.store.lists[2].header} 
+    cards={cardList3}
+  />
+       <List
+    
+    header={props.store.lists[3].header} 
+    cards={cardList4}
+  />
 
     </div>
+    </main>
   );
 };
 
