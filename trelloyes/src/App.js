de7c35926ac0643f35b1adbc1b90ee670ecc2251
James Lee <jameswlee109@gmail.com>
2019-09-03 15:45:02 -0500

    need all cards to populate

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -5,51 +5,37 @@ import STORE from './STORE';
 
 
 function App(props) {
+  console.log(props.store)
   
   //build cardlist
-  const cardList1 = props.store.lists[0].cardIds.map(id => {
-     return props.store.allCards[id];
-  });
-  const cardList2 = props.store.lists[1].cardIds.map(id => {
-    return props.store.allCards[id];
- });
- const cardList3 = props.store.lists[2].cardIds.map(id => {
-  return props.store.allCards[id];
-});
-const cardList4 = props.store.lists[3].cardIds.map(id => {
-  return props.store.allCards[id];
-});
+//   const cardList1 = props.store.lists[0].cardIds.map(id => {
+//      return props.store.allCards[id];
+//   });
+//   const cardList2 = props.store.lists[1].cardIds.map(id => {
+//     return props.store.allCards[id];
+//  });
+//  const cardList3 = props.store.lists[2].cardIds.map(id => {
+//   return props.store.allCards[id];
+// });
+// const cardList4 = props.store.lists[3].cardIds.map(id => {
+//   return props.store.allCards[id];
+// });
 
   return (
-    <main class="App">
-    <header class="App-header">
+    <main className="App">
+    <header className="App-header">
       <h1>Trelloyes!</h1>
     </header>
-    <div class="App-list">
-      
-      <List
-    
-        header={props.store.lists[0].header} 
-        cards={cardList1}
-      />
-      <List
-    
-    header={props.store.lists[1].header} 
-    cards={cardList2}
-  />
-     <List
-    
-    header={props.store.lists[2].header} 
-    cards={cardList3}
-  />
-       <List
-    
-    header={props.store.lists[3].header} 
-    cards={cardList4}
-  />
-
+    <div className="App-list">
+      {props.store.lists.map(list => (
+              <List
+                key={list.id}
+                header={list.header}
+                cards={list.cardIds.map(id => props.store.allCards[id])}
+              />
+    ))}
     </div>
-    </main>
+  </main>
   );
 };
 
